f0ff53dfebf908522248bc3b33a471c92a11b662
phhphc <phhphc@gmail.com>
2025-07-06 15:14:38 +0700

    update setup script

--- arch/setup.sh
+++ arch/setup.sh
@@ -4,11 +4,11 @@
 sudo pacman -Sy
 
 # setup sudo
-pacman -Sy --noconfirm sudo
+pacman -S --noconfirm sudo
 echo "%wheel ALL=(ALL:ALL) /usr/bin/pacman" >/etc/sudoers.d/wheel
 
 # setup ssh client
 if [ ! -f "$HOME/.ssh/id_ed25519" ]; then
-  sudo pacman -S openssh
+  sudo pacman -S --noconfirm openssh
   ssh-keygen -t ed25519 -C "$USER@$HOSTNAME"
 fi
